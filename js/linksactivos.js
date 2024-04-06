039ae5f40aebfc1d1167e16decf95aad0833fed4
Facundo Robert <robert.facundo@hotmail.com>
2024-04-06 20:49:47 -0300

    agregue estructuras y estilos a la mayoria de los html

---
+++ js/linksactivos.js
@@ -0,0 +1,15 @@
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Obtenemos todos los elementos <a> dentro de la lista <ul>
+//     const links = document.querySelectorAll("ul li a");
+  
+//     // Iteramos sobre cada enlace
+//     links.forEach(function (link) {
+//       // Comparamos la URL actual con la URL del enlace
+//       if (window.location.href === link.href) {
+//         // Si son iguales, agregamos la clase "active" al enlace
+//         link.classList.add("active");
+//       }
+//     });
+// });
